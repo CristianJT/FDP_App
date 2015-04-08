ebcdbb8e1dbc4b7754f645d2874a3bc140cafbbc
CristianJT <cjtodorovic@outlook.com>
2015-04-08 09:22:52 -0300

    Attribute routing

--- FDP_App/FDP_App/App_Start/WebApiConfig.cs
+++ FDP_App/FDP_App/App_Start/WebApiConfig.cs
@@ -16,13 +16,14 @@ namespace FDP_App
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
 
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
         }
     }
