e962f5eaccb3228a01e0c1d3a05c872a85cb958e
CristianJT <cjtodorovic@outlook.com>
2015-04-14 11:14:10 -0300

    Add equipos data

--- FDP_App/FDP_App/App/Services/appService.js
+++ FDP_App/FDP_App/App/Services/appService.js
@@ -7,9 +7,32 @@ appService.factory('appData', ['$resource', function ($resource) {
             { "id": 2, "nombre": "Primera División 2015", "descripcion": "Primer torneo de 30 equipos", "isCurrent": true, "equipos": [] }
     ];
 
+    var equipos = [ 
+            { "id": 1, "nombre": "Independiente", "estadio": "Libertadores de América" , "ubicacion": "Avellaneda(BsAs)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 2, "nombre": "River Plate", "estadio": "Antonio Vespucio Liberti" , "ubicacion": "Nuñez(CapFed)", "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 3, "nombre": "Boca Juniors", "estadio": "Alberto J. Armando" , "ubicacion": "La Boca(CapFed)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 4, "nombre": "Racing Club", "estadio": "Presidente Perón" , "ubicacion": "Avellaneda(BsAs)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 5, "nombre": "San Lorenzo", "estadio": "Pedro Bidegain" , "ubicacion": "Bajo Flores(CapFed)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 6, "nombre": "Huracan", "estadio": "Tomás Adolfo Ducó" , "ubicacion": "Parque Patricios(CapFed)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 7, "nombre": "Estudiantes(LP)" , "estadio": "Ciudad de La Plata" , "ubicacion": "La Plata(BsAs)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 8, "nombre": "Colon", "estadio": "Brigadier López" , "ubicacion": "Santa Fe(SFe)" , "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true }, 
+            { "id": 9, "nombre": "Quilmes", "estadio": "Centenario", "ubicacion": "Quilmes(BsAs)", "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": true },
+            { "id": 10, "nombre": "Gimnasia(J)", "estadio": "23 de Agosto", "ubicacion": "San Salvador(J)", "puntos": 0, "jugados": 0, "ganados": 0, "empatados": 0, "perdidos": 0, "golesFavor": 0, "golesContra": 0, "diferencia": 0, "esPrimera": false }
+    ];
+
     return {
         getTorneos: function() {
             return torneos;
+        },
+        getEquipos: function() {
+            return equipos;
+        },
+        getEquiposById: function (teamId) {
+            for (i = 0; i < equipos.length; i++) {
+                if (equipos[i].id == teamId) {
+                    return equipos[i];
+                }
+            }
         }
     }
     
