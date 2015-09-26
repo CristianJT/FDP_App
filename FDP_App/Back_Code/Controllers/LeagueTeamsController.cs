327c162579b4ffee24c02bd5b92ec0da528a8006
CristianJT <cjtodorovic@outlook.com>
2015-09-26 20:41:33 -0300

    Remake leagueTeams controller

--- FDP_App/Back_Code/Controllers/LeagueTeamsController.cs
+++ FDP_App/Back_Code/Controllers/LeagueTeamsController.cs
@@ -1,65 +1,102 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 
 namespace App.FDP
 {
-    [RoutePrefix("api/leagues/{leagueId}/teams")]
+    [RoutePrefix("api/torneos/{idTorneo}/equipos")]
     public class LeagueTeamsController : ApiController
     {
         private FDPAppContext db = new FDPAppContext();
 
-        /* GET: api/leagues/{leagueId}/teams */
+        /* GET: api/torneos/{idTorneo}/equipos */
         [Route("")]
-        [ResponseType(typeof(TeamsLeagueDTO))]
-        public IHttpActionResult GetLeagueTeams(int leagueId)
+        [HttpGet]
+        public IHttpActionResult GetLeagueTeams(int idTorneo)
         {
-            var teams = db.LeagueTeams.Where(t => t.LeagueId == leagueId).ToArray();
-            return Ok(teams.Select(t => new TeamsLeagueDTO(t)).ToArray());
+            LeagueTeam[] equipos = db.LeagueTeams.Where(lt => lt.LeagueId == idTorneo)
+                .AsNoTracking().ToArray();
+            return Ok(equipos.Select(t => new TeamLeagueDTO(t)));
         }
 
-        /* GET: api/leagues/{leagueId}/teams/{teamId} */
-        [Route("{teamId}")]
-        [ResponseType(typeof(TeamsLeagueDTO))]
-        public IHttpActionResult GetLeagueTeam(int leagueId, int teamId)
+        /* GET: api/torneos/{idTorneo}/equipos/{idEquipo} */
+        [Route("{idEquipo}")]
+        [HttpGet]
+        public IHttpActionResult GetLeagueTeam(int idTorneo, int idEquipo)
         {
-            var team = db.LeagueTeams.Where(t =>  t.LeagueId == leagueId && t.TeamId == teamId).FirstOrDefault();
-            if (team == null)
+            LeagueTeam equipo = db.LeagueTeams
+                .Where(lt => lt.LeagueId == idTorneo & lt.TeamId == idEquipo).AsNoTracking().FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(new TeamLeagueDTO(equipo));
+        }
+
+        /* POST: api/torneos/{idTorneo}/equipos/crear */
+        [Route("crear")]
+        [HttpPost]
+        public IHttpActionResult CreateLeagueTeam(int idTorneo, TeamIdsDTO equiposId )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            League torneo = db.Leagues
+                .Where(l => l.Id == idTorneo).FirstOrDefault();
+            if (torneo == null)
             {
                 return NotFound();
             }
 
-            return Ok(new TeamsLeagueDTO(team));
+            Team[] equipos = db.Teams
+                .Where(t => equiposId.ids.Contains(t.Id)).ToArray();
+
+            foreach (Team e in equipos)
+            {
+                LeagueTeam equipoTorneo = new LeagueTeam();
+                equipoTorneo.League = torneo;
+                equipoTorneo.Team = e;
+                if (!db.LeagueTeams.Any(lt => lt.LeagueId == idTorneo & lt.TeamId == e.Id))
+                {
+                    db.LeagueTeams.Add(equipoTorneo);
+                }             
+            }
+           
+            db.SaveChanges();
+
+            return Ok();
         }
 
-        /* PUT: api/leagues/{leagueId}/teams/{teamId} */
-        [Route("{teamId}")]
-        [ResponseType(typeof(TeamsLeagueDTO))]
+        /* PUT: api/torneos/{idTorneo}/equipos/{idEquipo} */
+        [Route("{idEquipo}")]
         [HttpPut]
-        public IHttpActionResult UpdateLeagueTeam(int leagueId, int teamId, TeamsLeagueDTO teamDto)
+        public IHttpActionResult UpdateLeagueTeam(int idTorneo, int idEquipo, TeamLeagueDTO equipoDTO)
         {
-            if (teamId != teamDto.TeamId || leagueId != teamDto.LeagueId)
+            if (idEquipo != equipoDTO.equipo_id || idTorneo != equipoDTO.torneo_id)
             {
                 return BadRequest();
             }
 
-            var lt = db.LeagueTeams.Where(x => x.LeagueId == leagueId && x.TeamId == teamId).FirstOrDefault();
-            if (lt == null)
+            var equipo = db.LeagueTeams.Where(x => x.LeagueId == idTorneo && x.TeamId == idEquipo).FirstOrDefault();
+            if (equipo == null)
             {
                 return NotFound();
             }
+            equipo.Played = equipoDTO.jugados;
+            equipo.Points = equipoDTO.puntos;
+            equipo.Won = equipoDTO.ganados;
+            equipo.Draws = equipoDTO.empatados;
+            equipo.Lost = equipoDTO.perdidos;
+            equipo.GoalsFor = equipoDTO.goles_favor;
+            equipo.GoalsAgainst = equipoDTO.goles_contra;
+            equipo.GoalDifference = equipoDTO.goles_diferencia;
 
-            lt.Played = teamDto.Played;
-            lt.Points = teamDto.Points;
-            lt.Won = teamDto.Won;
-            lt.Draws = teamDto.Draws;
-            lt.Lost = teamDto.Lost;
-            lt.GoalsFor = teamDto.GoalsFor;
-            lt.GoalsAgainst = teamDto.GoalsAgainst;
-            lt.GoalDifference = teamDto.GoalDifference;
             db.SaveChanges();
 
-            return Ok(new TeamsLeagueDTO(lt));
+            return Ok(new TeamLeagueDTO(equipo));
 
         }
     }
